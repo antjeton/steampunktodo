35bac4e06ecfbf8bff7c6e16b4cf706fe2bdffdf
antjeton <antjeton@yahoo.com>
2017-09-23 21:24:47 +0100

    first commit

---
+++ resources/js.js
@@ -0,0 +1,141 @@
+//alert(" is js working?");
+document.getElementById('datewindow').innerHTML=Date();
+
+
+
+var todoList = {
+  todos: [],
+  addTodo: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+  },
+  changeTodo: function(position, todoText) {
+    this.todos[position].todoText = todoText;
+  },
+  deleteTodo: function(position) {
+    this.todos.splice(position, 1);
+  },
+  toggleCompleted: function(position) {
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
+  },
+  toggleAll: function() {
+    var totalTodos = this.todos.length;
+    var completedTodos = 0;
+
+    this.todos.forEach(function(todo){
+      if(todo.completed ===true){
+        completedTodos ++;
+      }
+    });
+    this.todos.forEach(function(todo){
+      if( completedTodos=== totalTodos){
+        todo.completed=false;
+      }else{
+        todo.completed=true;
+      }
+    });
+  }
+};
+
+
+    // Get number of completed todos.
+  /*  for (var i = 0; i < totalTodos; i++) {
+      if (this.todos[i].completed === true) {
+        completedTodos++;
+      }
+    }
+
+    // Case 1: If everything’s true, make everything false.
+    if (completedTodos === totalTodos) {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = false;
+      }
+    // Case 2: Otherwise, make everything true.
+    } else {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = true;
+      }
+    }
+  }
+};*/
+
+var handlers = {
+  addTodo: function() {
+    var addTodoTextInput = document.getElementById('addTodoTextInput');
+    todoList.addTodo(addTodoTextInput.value);
+    addTodoTextInput.value = '';
+    view.displayTodos();
+
+  },
+  changeTodo: function() {
+    var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
+    var changeTodoTextInput = document.getElementById('changeTodoTextInput');
+    todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
+    changeTodoPositionInput.value = '';
+    changeTodoTextInput.value = '';
+    view.displayTodos();
+  },
+  deleteTodo: function(position) {
+
+    todoList.deleteTodo(position);
+
+    view.displayTodos();
+  },
+
+  toggleCompleted: function() {
+    var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
+    todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+    toggleCompletedPositionInput.value = '';
+    view.displayTodos();
+  },
+  toggleAll: function() {
+    todoList.toggleAll();
+    view.displayTodos();
+  }
+};
+
+var view = {
+  displayTodos: function() {
+    var todosUl = document.querySelector('ul');
+    todosUl.innerHTML = '';
+
+
+  //  for (var i = 0; i < todoList.todos.length; i++)
+       todoList.todos.forEach(function(todo,position) {
+       var todoLi = document.createElement('li');
+     // var todo = todoList.todos[i];
+      var todoTextWithCompletion = '';
+
+      if (todo.completed === true) {
+        todoTextWithCompletion = '(done) ' + todo.todoText;
+      } else {
+        todoTextWithCompletion = '( ) ' + todo.todoText;
+      }
+
+      todoLi.id= position;
+      todoLi.textContent = todoTextWithCompletion;
+       todoLi.appendChild(this.createDeleteButton());
+      todosUl.appendChild(todoLi);
+    }, this);
+  },
+  createDeleteButton : function () {
+     var deleteButton= document.createElement("button");
+     deleteButton.textContent = " (delete)";
+     deleteButton.className = "deleteButton";
+     return deleteButton;
+   },
+    setupEventListeners: function(){
+        var todosUl = document.querySelector ("ul");
+         todosUl.addEventListener("click", function(event){
+          var elementClicked=event.target;
+            if(elementClicked.className==="deleteButton"){
+           handlers.deleteTodo(parseInt(elementClicked.parentNode.id));
+        }
+      });
+   }
+ };
+
+view.setupEventListeners();
